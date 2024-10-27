0c19107095da4548cac3ddcfc4c7a6295642b2fe
Sam A <swaminathana@augray.com>
2024-10-27 19:56:17 +0530

    url & forms update

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for littlelemon project.
+"""littlelemon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingViewSet
+from restaurant import views
+
+router = DefaultRouter()
+
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', include('restaurant.urls')),
+    path('restaurant/',include('restaurant.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('restaurant/booking/', include(router.urls)),
 ]
